06ed3a6840dbeb49afc1b3cc9a94cd02596c8efa
JaiRaga <raga.j.santhosh@gmail.com>
2019-10-28 17:14:40 +0530

    Implementing react router

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// console.log(path.join(__dirname, 'public'))
+
+module.exports = (env) => {
+    const isProduction = env === 'production'
+
+    return {
+        entry: './src/app.js',
+        output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "styles.css"
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    }
+}
